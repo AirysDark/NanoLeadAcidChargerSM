0884604b22224959f6eb0ab1e8b81aac2bfe7b26
AirysDark <47623717+AirysDark@users.noreply.github.com>
2026-09-18 03:19:40 +1000

    Add browser firmware update server

---
+++ FirmwareUpdate.h
@@ -0,0 +1,38 @@
+#pragma once
+#include <Arduino.h>
+#include <ESP8266WebServer.h>
+#include "NanoFirmwareUpdater.h"
+
+class NanoLink;
+
+class FirmwareUpdate {
+public:
+  explicit FirmwareUpdate(NanoLink& nanoLink);
+
+  void begin(ESP8266WebServer& server);
+  void update();
+
+private:
+  ESP8266WebServer* _server;
+  NanoFirmwareUpdater _nanoUpdater;
+
+  bool _espUploadStarted;
+  bool _espUploadOk;
+  String _espUploadError;
+
+  bool _nanoUploadStarted;
+  bool _nanoUploadOk;
+  String _nanoUploadError;
+
+  bool _restartPending;
+  unsigned long _restartAtMs;
+
+  void handlePage();
+  void handleEspUpload();
+  void handleEspFinished();
+  void handleNanoUpload();
+  void handleNanoFinished();
+
+  void sendResult(bool ok, const String& message);
+  static String jsonEscape(const String& input);
+};
