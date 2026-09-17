38765cdacb99093da2b6e5105109f183331e9079
AirysDark <47623717+AirysDark@users.noreply.github.com>
2026-09-17 14:13:56 +1000

    Add ESP8266 debug header

---
+++ Debug.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <Arduino.h>
+
+namespace Debug {
+  void begin();
+  void print(const __FlashStringHelper* message);
+  void println(const __FlashStringHelper* message);
+  void println(const String& message);
+  void wifiInfo(const char* label, const IPAddress& ip);
+}
