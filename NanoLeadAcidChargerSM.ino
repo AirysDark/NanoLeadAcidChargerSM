95107efa1c90b4cdb094b0a1acce36121804e13d
AirysDark <47623717+AirysDark@users.noreply.github.com>
2026-09-17 14:13:37 +1000

    Add ESP8266 serial monitor main sketch

---
+++ NanoLeadAcidChargerSM.ino
@@ -0,0 +1,23 @@
+#include "Config.h"
+#include "Debug.h"
+#include "NanoLink.h"
+#include "NetworkManager.h"
+#include "WebUi.h"
+
+NanoLink nanoLink;
+NetworkManager network;
+WebUi webUi(nanoLink, network);
+
+void setup() {
+  Debug::begin();
+  nanoLink.begin();
+  network.begin();
+  webUi.begin();
+}
+
+void loop() {
+  nanoLink.update();
+  network.update();
+  webUi.update();
+  yield();
+}
