e4318d361d63d006747de509ce99410248028178
AirysDark <47623717+AirysDark@users.noreply.github.com>
2026-09-17 14:14:05 +1000

    Add ESP8266 debug implementation

---
+++ Debug.cpp
@@ -0,0 +1,31 @@
+#include "Debug.h"
+#include "Config.h"
+
+namespace Debug {
+
+void begin() {
+  if (!ENABLE_DEBUG) return;
+  Serial.begin(DEBUG_BAUD);
+  Serial.println();
+  Serial.println(F("NanoLeadAcidChargerSM ESP8266 starting"));
+}
+
+void print(const __FlashStringHelper* message) {
+  if (ENABLE_DEBUG) Serial.print(message);
+}
+
+void println(const __FlashStringHelper* message) {
+  if (ENABLE_DEBUG) Serial.println(message);
+}
+
+void println(const String& message) {
+  if (ENABLE_DEBUG) Serial.println(message);
+}
+
+void wifiInfo(const char* label, const IPAddress& ip) {
+  if (!ENABLE_DEBUG) return;
+  Serial.print(label);
+  Serial.println(ip);
+}
+
+}  // namespace Debug
